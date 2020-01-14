54255923d455c1b822da8a9375d1938e9f4dbf9c
Shagrid <shagrid@yandex.ru>
2020-01-14 22:54:10 +0300

    Реализован заряд батареи. Батарейка на месности

---
+++ Assets/Scripts/Model/BatteryModel.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Geekbrains
+{
+    public sealed class BatteryModel : BaseMakeUsingModel
+    {
+        private FlashLightModel _flashLight;
+        [SerializeField]private float _charge = 10;
+
+        protected override void Awake()
+        {
+            _flashLight = Object.FindObjectOfType<FlashLightModel>();
+        }
+
+        public override void MakeUsing()
+        {
+            _flashLight.BatteryCharging(_charge);
+        }
+    }
+}
