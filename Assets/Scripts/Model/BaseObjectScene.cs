9672ae33e29074df550ea422d24ae0b3d71551dc
Shagrid <shagrid@yandex.ru>
2020-01-15 18:51:31 +0300

    Выключение видимости объекта

--- Assets/Scripts/Model/BaseObjectScene.cs
+++ Assets/Scripts/Model/BaseObjectScene.cs
@@ -5,6 +5,7 @@ namespace Geekbrains
     public abstract class BaseObjectScene : MonoBehaviour
     {
         private int _layer;
+        private bool _isVisible;
         public Rigidbody Rigidbody { get; private set; }
         public Transform Transform { get; private set; }
         public int Layer
@@ -27,6 +28,31 @@ namespace Geekbrains
                 AskLayer(child, layer);
             }
         }
+
+        public bool IsVisible
+        {
+            get => _isVisible;
+            set
+            {
+                _isVisible = value;
+                var renderer = GetComponent<Renderer>();
+                if (renderer)
+                    renderer.enabled = _isVisible;
+                AskVisible(Transform, value);
+            }
+        }
+
+        private void AskVisible(Transform obj, bool value)
+        {
+            if (Transform.childCount <= 0) return;
+            foreach (Transform t in transform)
+            {
+                var renderer = t.gameObject.GetComponent<Renderer>();
+                if (renderer)
+                    renderer.enabled = _isVisible;
+            }
+        }
+
 
         protected virtual void Awake()
         {
