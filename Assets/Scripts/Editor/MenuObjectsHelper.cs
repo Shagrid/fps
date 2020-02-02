f97bc91f6fd0c306096a6108c574844d8f657f3a
Shagrid <shagrid@yandex.ru>
2020-02-02 17:01:29 +0300

    Меню GameDesigherHelper, окно AddObjectsToScene, расстановка объектов по сцене.

---
+++ Assets/Scripts/Editor/MenuObjectsHelper.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+
+namespace Geekbrains.Editor
+{
+    
+    public class MenuObjectsHelper : UnityEditor.Editor
+    {
+        [MenuItem("GameDesignerHelper/Add Objects To Scene")]
+        public static void CreateObjectsWindow()
+        {
+            EditorWindow.GetWindow(typeof(CreateObjectsWindow), false, "Add objects to scene");
+        }
+    }
+}
